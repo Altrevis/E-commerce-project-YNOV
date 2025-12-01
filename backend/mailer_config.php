f1577b42336625a95b10f3faeeb7ffabd1b1b551
Alex Kedevil <alexandre.queudeville@gmail.com>
2025-12-01 12:59:01 +0100

    smtp fix

--- backend/mailer_config.php
+++ backend/mailer_config.php
@@ -1,7 +1,7 @@
 <?php
-define('SMTP_HOST', 'smtp.yandex.com');
+define('SMTP_HOST', 'smtp.ynov-commerce.com');
 define('SMTP_PORT', 587);
-define('SMTP_USER', 'queudeville.alexandre@yandex.ru');
-define('SMTP_PASS', 'dedcfdyopyvohuwu');
-define('SMTP_FROM_EMAIL', 'queudeville.alexandre@yandex.ru');
+define('SMTP_USER', 'no-reply@ynov-commerce.com');
+define('SMTP_PASS', 'xxxxxxxx');
+define('SMTP_FROM_EMAIL', 'no-reply@ynov-commerce.com');
 define('SMTP_FROM_NAME', 'E-commerce YNOV');
